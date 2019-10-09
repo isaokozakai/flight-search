67375b0a48a4d34e706ca85f25b724595b55e21d
Isao Kozakai <feat130@hotmail.co.jp>
2019-10-09 12:03:44 -0700

    Add autocomplete function for the names of the airports

--- js/index.js
+++ js/index.js
@@ -1,4 +1,32 @@
-$(function () {
+$(() => {
   $("#departure-date").datepicker();
   $("#return-date").datepicker();
+
+  $("#from, #to").autocomplete({
+    source: function (req, res) {
+      $.ajax({
+        "async": true,
+        "crossDomain": true,
+        "data": `text=${req.term}`,
+        "url": 'https://cometari-airportsfinder-v1.p.rapidapi.com/api/airports/by-text',
+        "method": "GET",
+        "headers": {
+          "x-rapidapi-host": "cometari-airportsfinder-v1.p.rapidapi.com",
+          "x-rapidapi-key": "9e17e55ef7msh7b448d0f0cae1b2p13d1cfjsn95340acc8e72"
+        }
+      }).done((data) => res(
+        data.map(
+          (airport) => ({
+            label: `${airport.name} (${airport.code})`,
+            value: `${airport.name} (${airport.code})`,
+            code: airport.code
+          })
+        )
+      ));
+    },
+    select: function (e, ui) {
+      console.log(ui);
+    }
+  });
+
 });
