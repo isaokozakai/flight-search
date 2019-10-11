50a8a0ca487a0eb6f44bae014d8fca2666b57f5e
Isao Kozakai <feat130@hotmail.co.jp>
2019-10-10 20:15:08 -0700

    Update the searching flights  function

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 $(() => {
   $("#departureDate").datepicker();
   $("#returnDate").datepicker();
-
   $("#from, #to").autocomplete({
     maxShowItems: 5,
     source: (req, res) => {
@@ -30,45 +29,130 @@ $(() => {
     }
   });
 
+
   $("#inputForm").submit((e) => {
     e.preventDefault();
     $("#result").empty();
     const [from, to, departureDate, returnDate] = e.target.elements;
-    const originplace = $(from)[0].attributes.code.value;
-    const destinationplace = $(to)[0].attributes.code.value;
-    const outbounddate = $.datepicker.formatDate( "yy-mm-dd", new Date(departureDate.value));
-    const inbounddate = $.datepicker.formatDate( "yy-mm-dd", new Date(returnDate.value));
-    $.ajax({
+    const originPlace = $(from)[0].attributes.code.value;
+    const destinationPlace = $(to)[0].attributes.code.value;
+    const outboundDate = $.datepicker.formatDate("yy-mm-dd", new Date(departureDate.value));
+    const inboundDate = $.datepicker.formatDate("yy-mm-dd", new Date(returnDate.value));
+
+    const setting = {
       "async": true,
       "crossDomain": true,
-      "url": `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/${originplace}/${destinationplace}/${outbounddate}?inboundpartialdate=${inbounddate}`,
-      "method": "GET",
+      "url": "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/v1.0",
+      "method": "POST",
       "headers": {
         "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-        "x-rapidapi-key": "9e17e55ef7msh7b448d0f0cae1b2p13d1cfjsn95340acc8e72"
+        "x-rapidapi-key": "9e17e55ef7msh7b448d0f0cae1b2p13d1cfjsn95340acc8e72",
+        "content-type": "application/x-www-form-urlencoded"
+      },
+      "data": {
+        inboundDate,
+        originPlace,
+        destinationPlace,
+        outboundDate,
+        "cabinClass": "economy",
+        "children": "0",
+        "infants": "0",
+        "country": "US",
+        "currency": "USD",
+        "locale": "en-US",
+        "adults": "1"
       }
-    }).done((response) => {
-      if (response.Quotes.length === 0) {
-        console.log("no flight")
-      } else {
-        const directFlights = response.Quotes.filter((quote) => quote.Direct);
-        if (directFlights.length === 0) {
-          console.log("no direct")
-        } else {
-          const carrierNames = response.Carriers.map((carrier) => ({ [carrier.CarrierId]: carrier.Name }));
+    };
 
-          directFlights.forEach((flight) => {
-            const carrierId = flight.OutboundLeg.CarrierIds[0];
-            const carrier = response.Carriers.find((carrier) => carrier.CarrierId === carrierId)
-            $("#result").append(`
-            <div>
-              ${flight.MinPrice}, ${carrier.Name}
-            </div>
-            `)
+    $.ajax(setting).done((response) => {
+      console.log(response);
+    }).always((data, textStatus, jqXHR) => {
+      const sessionkey = jqXHR.getResponseHeader("location").slice(-36);
+      const setting = {
+        "async": true,
+        "crossDomain": true,
+        "url": `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/uk2/v1.0/${sessionkey}`,
+        "method": "GET",
+        "headers": {
+          "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+          "x-rapidapi-key": "9e17e55ef7msh7b448d0f0cae1b2p13d1cfjsn95340acc8e72"
+        },
+        "data": {
+          "sortType": "price",
+          "sortOrder": "asc"
+        }
+      };
+
+      $.ajax(setting).done((response) => {
+        console.log(response);
+        if (response.Itineraries.length === 0) {
+          console.log("no flight")
+        } else {
+          response.Itineraries.forEach((itinerary) => {
+            const minPrice = itinerary.PricingOptions[0].Price;
+
+            const outboundLeg = response.Legs.find((leg) => leg.Id === itinerary.OutboundLegId);
+            const outboundSegments = outboundLeg.SegmentIds.map((segmentId) =>
+              response.Segments.find((segment) => segment.Id === segmentId)
+            );
+
+            const outboundInfo = outboundSegments.map((segment) => {
+              const origin = response.Places.find((place) => place.Id === segment.OriginStation);
+              const destination = response.Places.find((place) => place.Id === segment.DestinationStation);
+              const carrier = response.Carriers.find((carrier) => carrier.Id === segment.Carrier);
+              return ({
+                originId: origin.Id,
+                originCode: origin.Code,
+                originName: origin.Name,
+                departureDateTime: segment.DepartureDateTime,
+                destinationId: destination.Id,
+                destinationCode: destination.Code,
+                destinationName: destination.Name,
+                arrivalDateTime: segment.ArrivalDateTime,
+                carrier
+              });
+            });
+
+            outboundInfo.sort((a, b) => a.departureDateTime < b.departureDateTime ? -1 : 1);
+
+            const inboundLeg = response.Legs.find((leg) => leg.Id === itinerary.InboundLegId);
+            const inboundSegments = inboundLeg.SegmentIds.map((segmentId) =>
+              response.Segments.find((segment) => segment.Id === segmentId)
+            );
+
+            const inboundInfo = inboundSegments.map((segment) => {
+              const origin = response.Places.find((place) => place.Id === segment.OriginStation);
+              const destination = response.Places.find((place) => place.Id === segment.DestinationStation);
+              const carrier = response.Carriers.find((carrier) => carrier.Id === segment.Carrier);
+              return ({
+                originId: origin.Id,
+                originCode: origin.Code,
+                originName: origin.Name,
+                departureDateTime: segment.DepartureDateTime,
+                destinationId: destination.Id,
+                destinationCode: destination.Code,
+                destinationName: destination.Name,
+                arrivalDateTime: segment.ArrivalDateTime,
+                carrier
+              });
+            });
+
+            inboundInfo.sort((a, b) => a.departureDateTime < b.departureDateTime ? -1 : 1);
+
+            $("#result").append($("<div></div>").addClass("itinerary"));
+            $(".itinerary").last().append($("<div></div>").text(`${minPrice} USD `));
+
+            for (let i = 0; i < outboundInfo.length; i++) {
+              $(".itinerary").last().append($("<div></div>")
+                .text(`
+                  ${outboundInfo[i].carrier.Name} 
+                  ${outboundInfo[i].departureDateTime}, ${outboundInfo[i].originCode},
+                  ${outboundInfo[i].arrivalDateTime}, ${outboundInfo[i].destinationCode}
+                `));
+            }
           });
         }
-      }
+      });
     });
-  }
-  );
+  });
 });
