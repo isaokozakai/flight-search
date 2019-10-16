d0f200bc7c18666a7f589e3c908b76df46b6d893
Isao Kozakai <feat130@hotmail.co.jp>
2019-10-16 12:20:52 -0700

    Make the regional info modal work

--- js/index.js
+++ js/index.js
@@ -1,211 +1,3 @@
-$(() => {
-  // create a dropdown for countries
-  $.ajax({
-    ...commonSetting,
-    "url": "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/reference/v1.0/countries/en-US"
-  }).done(function (response) {
-    response.Countries.sort((a, b) => a.Name < b.Name ? -1 : 1)
-      .forEach((countries) => {
-        $("#countries").append($("<option>", { code: countries.Code }).text(countries.Name));
-      });
-    // set a default value
-    $("#countries").val("United States");
-  });
-
-  // create a dropdown for currencies
-  $.ajax({
-    ...commonSetting,
-    "url": "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/reference/v1.0/currencies"
-  }).done(function (response) {
-    response.Currencies.sort((a, b) => a.Code < b.Code ? -1 : 1)
-      .forEach((currency) => {
-        $("#currencies").append($("<option>").text(currency.Code));
-      });
-    // set a default value
-    $("#currencies").val("USD");
-  });
-
-  $("#departureDate").datepicker();
-  $("#returnDate").datepicker();
-
-  $("#from, #to").autocomplete({
-    source: (req, res) => {
-      $.ajax({
-        ...commonSetting,
-        "data": `query=${req.term}`,
-        "url": "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/US/USD/en-US/"
-      }).done((data) => res(
-        data.Places
-          .filter((place) => place.PlaceId != place.CountryId)
-          .map(
-            (place) => {
-              const placeId = place.PlaceId.slice(0, -4);
-              const cityId = place.CityId.slice(0, -4);
-              const modifier = placeId == cityId ? "Any" : placeId;
-              return ({
-                label: `${place.PlaceName} (${modifier})`,
-                value: `${place.PlaceName} (${modifier})`,
-                code: place.PlaceId
-              })
-            }
-          )
-      ));
-    },
-    select: (e, ui) => {
-      $(e.target).attr("code", ui.item.code);
-    }
-  });
-
-  const $travelersField = $("#travelers");
-  const $travelersBalloon = $("#travelers-balloon");
-
-  // place a balloon for travelers
-  const posision = $travelersField.offset();
-  $travelersBalloon.css({ top: posision.top + 37, left: posision.left });
-
-  $travelersField.on("click touchstart", (e) => {
-    $travelersBalloon.toggle();
-  });
-
-  const $regionalInfoBtn = $("#regionalInfo");
-  const $modalBackground = $(".modal-overlay");
-  const $modal = $(".modal");
-
-  $regionalInfoBtn.on("click touchstart", (e) => {
-    $regionalInfoBtn.css({ outline: "none" });
-    $modalBackground.css({ display: "block" });
-    $modal.css({ display: "block" });
-  });
-
-  $(document).on("click touchstart", (e) => {
-    if (
-      $travelersBalloon.css("display") == "block"
-      && !($(e.target).is($travelersField) || $(e.target).closest($travelersBalloon)[0])
-    ) {
-      $travelersBalloon.css({ display: "none" });
-    }
-    if ($(e.target).is($modalBackground)) {
-      $modalBackground.css({ display: "none" });
-      $modal.css({ display: "none" });
-    }
-  });
-
-  $(".close").click((e) => {
-    $modalBackground.css({ display: "none" });
-    $modal.css({ display: "none" });
-  })
-
-  $("#adults, #children, #infants").change((e) => {
-    const adults = parseInt($("#adults").val());
-    const children = parseInt($("#children").val());
-    const infants = parseInt($("#infants").val());
-    const total = adults + children + infants;
-    let modifier = total > 1 ? "travelers" : "adult";
-
-    // set a value for the display
-    $("#travelers").val(total + " " + modifier)
-    // set values for the API
-    $("#travelers").attr({ adults, children, infants });
-  })
-
-  $(".minus").click(function (e) {
-    const $inputField = $(this).next();
-    const $plusButton = $(this).next().next();
-    const value = parseInt($inputField.val());
-
-    if ((($inputField.attr("id") == "adults") && value == 2) || value == 1) {
-      $(this).prop("disabled", true);
-    } else {
-      $plusButton.prop("disabled", false);
-    }
-    $inputField.val(value - 1);
-
-    // trigger the change event for the number of each traveler
-    $inputField.trigger("change");
-  });
-
-  $(".plus").click(function (e) {
-    const $inputField = $(this).prev();
-    const $minusButton = $(this).prev().prev();
-    const value = parseInt($inputField.val());
-
-    if (value == 7) {
-      $(this).prop("disabled", true);
-    } else {
-      $minusButton.prop("disabled", false);
-    }
-    $inputField.val(value + 1);
-
-    // trigger the change event for the number of each traveler
-    $inputField.trigger("change");
-  });
-
-  $("#inputForm").submit((e) => {
-    e.preventDefault();
-    $("#result").empty();
-    const inputItems = e.target.elements;
-    const originPlace = inputItems.from.attributes.code.value;
-    const destinationPlace = inputItems.to.attributes.code.value;
-    const outboundDate = $.datepicker.formatDate("yy-mm-dd", new Date(inputItems.departureDate.value));
-    const inboundDate = $.datepicker.formatDate("yy-mm-dd", new Date(inputItems.returnDate.value));
-    const cabinClass = inputItems.cabinClass.value;
-    const adults = inputItems.travelers.attributes.adults.value;
-    const children = inputItems.travelers.attributes.children.value;
-    const infants = inputItems.travelers.attributes.infants.value;
-    const country = inputItems.countries.attributes.code;
-    const currency = inputItems.currencies.value;
-
-    const createSessionSetting = {
-      ...commonSetting,
-      "url": "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/v1.0",
-      "method": "POST",
-      "headers": {
-        ...commonSetting.headers,
-        "content-type": "application/x-www-form-urlencoded"
-      },
-      "data": {
-        inboundDate,
-        originPlace,
-        destinationPlace,
-        outboundDate,
-        cabinClass,
-        adults,
-        children,
-        infants,
-        country,
-        currency,
-        "groupPricing": true,
-        "locale": "en-US"
-      }
-    };
-
-    $.ajax(createSessionSetting).done((response) => {
-      console.log("done for Create session", response);
-
-    }).fail((jqXHR, textStatus, errorThrown) => {
-      console.log("fail for Create session", jqXHR, textStatus, errorThrown);
-
-    }).always((data, textStatus, jqXHR) => {
-      console.log("always for Create session", data, textStatus, jqXHR);
-
-      const sessionkey = jqXHR.getResponseHeader("location").slice(-36);
-      const pollSessionResultSetting = {
-        ...commonSetting,
-        "url": `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/uk2/v1.0/${sessionkey}`,
-        "data": {
-          "sortType": "price",
-          "sortOrder": "asc",
-          "stops": "1",
-          "pageIndex": "0",
-          "pageSize": "20"
-        }
-      };
-
-      pollSessionResult(pollSessionResultSetting);
-    });
-  });
-});
-
 const commonSetting = {
   "async": true,
   "crossDomain": true,
@@ -334,3 +126,226 @@ const dateFormatter = (value) => {
   const options = { hour: "numeric", minute: "numeric", year: "numeric", month: "short", day: "numeric" };
   return date.toLocaleString("en-US", options);
 };
+
+$(() => {
+  // create a dropdown for countries
+  $.ajax({
+    ...commonSetting,
+    "url": "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/reference/v1.0/countries/en-US"
+  }).done((response) => {
+    response.Countries.sort((a, b) => a.Name < b.Name ? -1 : 1)
+      .forEach((countries) => {
+        $("#countries").append($("<option>", { label: countries.Name }).val(countries.Code));
+      });
+    // set a default value to the input for the server
+    const defaultCountry = response.Countries.find((country) => country.Code == "US");
+    $("#country").val(defaultCountry.Code);
+  });
+
+  // create a dropdown for currencies
+  $.ajax({
+    ...commonSetting,
+    "url": "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/reference/v1.0/currencies"
+  }).done(function (response) {
+    response.Currencies.sort((a, b) => a.Code < b.Code ? -1 : 1)
+      .forEach((currency) => {
+        $("#currencies").append($("<option>").text(currency.Code));
+      });
+    // set a default value to the input for the server
+    const defaultCurrency = response.Currencies.find((currency) => currency.Code == "USD");
+    $("#currency").val(defaultCurrency.Code);
+  });
+
+  $("#regionalInfo").on("click touchstart", ((e) => {
+    $("#countries").val($("#country").val());
+    $("#currencies").val($("#currency").val());
+  }));
+
+  $("#regionalInfoForm").submit((e) => {
+    e.preventDefault();
+    $("#country").val(e.target.countries.value);
+    $("#currency").val(e.target.currencies.value);
+    $modalBackground.css({ display: "none" });
+    $modal.css({ display: "none" });
+  });
+
+  $("#departureDate").datepicker();
+  $("#returnDate").datepicker();
+
+  $("#from, #to").autocomplete({
+    source: (req, res) => {
+      $.ajax({
+        ...commonSetting,
+        "data": `query=${req.term}`,
+        "url": "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/US/USD/en-US/"
+      }).done((data) => res(
+        data.Places
+          .filter((place) => place.PlaceId != place.CountryId)
+          .map(
+            (place) => {
+              const placeId = place.PlaceId.slice(0, -4);
+              const cityId = place.CityId.slice(0, -4);
+              const modifier = placeId == cityId ? "Any" : placeId;
+              return ({
+                label: `${place.PlaceName} (${modifier})`,
+                value: `${place.PlaceName} (${modifier})`,
+                code: place.PlaceId
+              })
+            }
+          )
+      ));
+    },
+    select: (e, ui) => {
+      $(e.target).attr({ code: ui.item.code });
+    }
+  });
+
+  const $regionalInfoBtn = $("#regionalInfo");
+  const $modalBackground = $(".modal-overlay");
+  const $modal = $(".modal");
+  const $travelersField = $("#travelers");
+  const $travelersBalloon = $("#travelers-balloon");
+
+  // place a balloon for travelers
+  const posision = $travelersField.offset();
+  $travelersBalloon.css({ top: posision.top + 37, left: posision.left });
+
+  $travelersField.on("click touchstart", (e) => {
+    $travelersBalloon.toggle();
+  });
+
+
+  $regionalInfoBtn.on("click touchstart", (e) => {
+    $regionalInfoBtn.css({ outline: "none" });
+    $modalBackground.css({ display: "block" });
+    $modal.css({ display: "block" });
+  });
+
+  $(document).on("click touchstart", (e) => {
+    if (
+      $travelersBalloon.css("display") == "block"
+      && !($(e.target).is($travelersField) || $(e.target).closest($travelersBalloon)[0])
+    ) {
+      $travelersBalloon.css({ display: "none" });
+    }
+    if ($(e.target).is($modalBackground)) {
+      $modalBackground.css({ display: "none" });
+      $modal.css({ display: "none" });
+    }
+  });
+
+  $(".close").click((e) => {
+    $modalBackground.css({ display: "none" });
+    $modal.css({ display: "none" });
+  })
+
+  $("#adults, #children, #infants").change((e) => {
+    const adults = parseInt($("#adults").val());
+    const children = parseInt($("#children").val());
+    const infants = parseInt($("#infants").val());
+    const total = adults + children + infants;
+    let modifier = total > 1 ? "travelers" : "adult";
+
+    // set a value for the display
+    $travelersField.val(total + " " + modifier)
+    // set values for the server
+    $travelersField.attr({ adults, children, infants });
+  })
+
+  $(".minus").click(function (e) {
+    const $inputField = $(this).next();
+    const $plusButton = $(this).next().next();
+    const value = parseInt($inputField.val());
+
+    if ((($inputField.attr("id") == "adults") && value == 2) || value == 1) {
+      $(this).prop("disabled", true);
+    } else {
+      $plusButton.prop("disabled", false);
+    }
+    $inputField.val(value - 1);
+
+    // trigger the change event for the number of each traveler
+    $inputField.trigger("change");
+  });
+
+  $(".plus").click(function (e) {
+    const $inputField = $(this).prev();
+    const $minusButton = $(this).prev().prev();
+    const value = parseInt($inputField.val());
+
+    if (value == 7) {
+      $(this).prop("disabled", true);
+    } else {
+      $minusButton.prop("disabled", false);
+    }
+    $inputField.val(value + 1);
+
+    // trigger the change event for the number of each traveler
+    $inputField.trigger("change");
+  });
+
+  $("#inputForm").submit((e) => {
+    e.preventDefault();
+    $("#result").empty();
+    const inputItems = e.target.elements;
+    const originPlace = inputItems.from.attributes.code.value;
+    const destinationPlace = inputItems.to.attributes.code.value;
+    const outboundDate = $.datepicker.formatDate("yy-mm-dd", new Date(inputItems.departureDate.value));
+    const inboundDate = $.datepicker.formatDate("yy-mm-dd", new Date(inputItems.returnDate.value));
+    const cabinClass = inputItems.cabinClass.value;
+    const adults = inputItems.travelers.attributes.adults.value;
+    const children = inputItems.travelers.attributes.children.value;
+    const infants = inputItems.travelers.attributes.infants.value;
+    const country = inputItems.country.value;
+    const currency = inputItems.currency.value;
+
+    const createSessionSetting = {
+      ...commonSetting,
+      "url": "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/v1.0",
+      "method": "POST",
+      "headers": {
+        ...commonSetting.headers,
+        "content-type": "application/x-www-form-urlencoded"
+      },
+      "data": {
+        inboundDate,
+        originPlace,
+        destinationPlace,
+        outboundDate,
+        cabinClass,
+        adults,
+        children,
+        infants,
+        country,
+        currency,
+        "groupPricing": true,
+        "locale": "en-US"
+      }
+    };
+
+    $.ajax(createSessionSetting).done((response) => {
+      console.log("done for Create session", response);
+
+    }).fail((jqXHR, textStatus, errorThrown) => {
+      console.log("fail for Create session", jqXHR, textStatus, errorThrown);
+
+    }).always((data, textStatus, jqXHR) => {
+      console.log("always for Create session", data, textStatus, jqXHR);
+
+      const sessionkey = jqXHR.getResponseHeader("location").slice(-36);
+      const pollSessionResultSetting = {
+        ...commonSetting,
+        "url": `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/uk2/v1.0/${sessionkey}`,
+        "data": {
+          "sortType": "price",
+          "sortOrder": "asc",
+          "stops": "1",
+          "pageIndex": "0",
+          "pageSize": "20"
+        }
+      };
+
+      pollSessionResult(pollSessionResultSetting);
+    });
+  });
+});
